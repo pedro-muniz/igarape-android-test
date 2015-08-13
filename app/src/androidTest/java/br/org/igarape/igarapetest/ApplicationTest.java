9d5890243ae63fb6944cd7aa966d59cbc7f00db6
Pedro Muniz <pedro.muniz@geeklab.com.br>
2015-08-12 22:07:45 -0300

    Teste para mostrar latitude e longitude na tela inicial

--- app/src/androidTest/java/br/org/igarape/igarapetest/ApplicationTest.java
+++ app/src/androidTest/java/br/org/igarape/igarapetest/ApplicationTest.java
@@ -1,12 +1,18 @@
 package br.org.igarape.igarapetest;
 
 import android.app.Application;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
 import android.test.ApplicationTestCase;
+import android.widget.TextView;
+
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
+public class ApplicationTest  extends ApplicationTestCase<Application>  {
     public ApplicationTest() {
         super(Application.class);
     }
--- app/src/main/java/br/org/igarape/igarapetest/MainActivity.java
+++ app/src/main/java/br/org/igarape/igarapetest/MainActivity.java
@@ -1,16 +1,32 @@
 package br.org.igarape.igarapetest;
 
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        String bestProvider = locationManager.getBestProvider(criteria, true);
+        Location location = locationManager.getLastKnownLocation(bestProvider);
+        if (location != null) {
+            onLocationChanged(location);
+        }
+
+        locationManager.requestLocationUpdates(bestProvider, 20000, 0, this);
     }
 
     @Override
@@ -34,4 +50,28 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void onLocationChanged(Location location) {
+        Location mCurrentLocation = location;
+        TextView txtLatitude = (TextView)findViewById(R.id.txtLatitudeValue);
+        TextView txtLongitude = (TextView)findViewById(R.id.txtLongitudeValue);
+
+        txtLatitude.setText(String.valueOf(mCurrentLocation.getLatitude()));
+        txtLongitude.setText(String.valueOf(mCurrentLocation.getLongitude()));
+    }
+
+
+    //@Override
+    public void onProviderEnabled(String provider) {}
+    public void onProviderDisabled(String provider) {}
+    public void onStatusChanged(String a, int b, Bundle c) {}
+    protected void stopLocationUpdates() {}
+    public void onResume() {super.onResume();}
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdates();
+    }
+
 }
